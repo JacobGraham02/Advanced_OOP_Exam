b1480cfc6dc631a922084c6f507a68b1645bf7af
nicGardner <nicgardner@rogers.com>
2018-12-14 13:02:18 -0500

    built InventoryView.fxml

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Inventoryview.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 825, 600));
         primaryStage.show();
     }
 
